50aa833e53a3a8fe481ee86c42f29cb6b0d7a22b
apchandler-byui <apchandler@byui.edu>
2024-05-21 14:43:59 -0700

    a

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+const products = [
+    {
+        id: 'fc-1888',
+        name: 'flux capacitor',
+        'avg-rating': 4.5
+    },
+    {
+        id: 'fc-2050',
+        name: 'power laces',
+        'avg-rating': 4.7
+    },
+    {
+        id: 'fs-1987',
+        name: 'time circuits',
+        'avg-rating': 3.5
+    },
+    {
+        id: 'ac-2000',
+        name: 'low voltage reactor',
+        'avg-rating': 3.9
+    },
+    {
+        id: 'jj-1969',
+        name: 'warp equalizer',
+        'avg-rating': 5.0
+    }
+];
+
+window.onload = function() {
+    const select = document.getElementById('product');
+    products.forEach(function(product) {
+        const option = document.createElement('option');
+        option.value = product.id;
+        option.text = product.name;
+        select.appendChild(option);
+    });
+};
+
+document.querySelector('form').addEventListener('submit', function() {
+    let count = localStorage.getItem('reviewCount') || 0;
+    localStorage.setItem('reviewCount', ++count);
+});
+
+var date = new Date(document.lastModified);
+document.getElementById('footer-content').textContent =  'This page was last modified on ' + date.toLocaleDateString() + '. Current year is ' + new Date().getFullYear() + '.';
